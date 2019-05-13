45bda87128aa4dd6f721d88f2f87a0d669ec1aa4
simonkcoding <ykwok3@uncc.edu>
2019-05-13 07:40:19 -0400

    5-13,7:40

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,5 +139,21 @@ router.get("/user/:user_id", async (req, res) => {
     }
 })
 
+//@route  Get api/profile/
+//@desc   get all profile
+//@access Public
+
+router.delete("/", auth, async (req, res) => {
+    try {
+        //@todo - remove users posts
+        //Remove profile
+        await Profile.findOneAndRemove({user:req.user.id});
+        await User.findOneAndRemove({_id:req.user.id});
+        res.json({msg:'User removed'});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('server error');
+    }
+})
 
 module.exports = router;
